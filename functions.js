4a152167d22a0baf77ea8c695ace731d290a2c69
Lekso4ka <lexysanke@gmail.com>
2023-12-31 19:19:54 +0300

    create pro

---
+++ functions.js
@@ -0,0 +1,86 @@
+const pluralize = (n, w0, w1, w2) => {
+    if (n % 100 < 11 || n % 100 > 14) {
+        if (n % 10 === 1) {
+            return w1;
+        } else if (n % 10 >= 2 && n % 10 <= 4) {
+            return w2;
+        }
+    }
+    return w0;
+}
+
+const getWordWithVowels = (n = 3, arr) => {
+    const vowels = ["а","е","ё","и","о","у","ы","э","ю","я"];
+    const manyVowels = []
+    for (let i = 0, cnt = vowels.length; i < cnt; i++) {
+        for (let j = i + 1; j < cnt; j++) {
+            if (n === 3) {
+                for (let k = j + 1; k < cnt; k++) {
+                    manyVowels.push([vowels[i], vowels[j], vowels[k]]);
+                }
+            } else {
+                manyVowels.push([vowels[i], vowels[j]]);
+            }
+        }
+    }
+    return manyVowels;
+}
+const getRandom = (max = 100, min = 0) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+const setCaption = (tag, arr) => {
+    tag.innerHTML = `Найдено <span>${arr.length}</span> ${pluralize(arr.length, "подходящих", "подходящее", "подходящих")} ${pluralize(arr.length, "слов", "слово", "слова")}`;
+}
+
+const setWords = (tag, arr) => {
+    const result = arr.map(w => `<span>${w}</span>`);
+    tag.innerHTML = result.join("");
+}
+
+const findWithVowels = (n = 3, arr) => {
+    const variants = getWordWithVowels(n);
+    const result = [];
+    variants.forEach(val => {
+        for (let i = 0, cnt = arr.length; i < cnt; i++) {
+            const word = arr[i];
+            let flag = true;
+            for (let j = 0; j < val.length; j++) {
+                if (word.includes(val[j])) continue;
+                flag = false;
+                break;
+            }
+            if (flag) {
+                result.push(word);
+            }
+        }
+    })
+    return result;
+}
+
+const findByChars = (arr) => {
+    const charsActiveF = document.getElementsByClassName("char_f active");
+    const charsActiveT = document.getElementsByClassName("char_t active");
+    let result = [];
+    arr.forEach(w => {
+        let flag = true;
+        for (let i = 0; i < charsActiveF.length; i++) {
+            const txt = charsActiveF[i].innerText.toLowerCase()
+            if (w.includes(txt)) {
+                flag = false;
+                break;
+            }
+        }
+        for (let i = 0; i < charsActiveT.length; i++) {
+            const txt = charsActiveT[i].innerText.toLowerCase()
+            if (!w.includes(txt)) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            result.push(w)
+        }
+    })
+    return result;
+}
---
+++ index.js
@@ -0,0 +1,87 @@
+const caption = document.querySelector(".caption")
+const maskTag = document.querySelector(".mask");
+const mask = maskTag.querySelector("input");
+const findBtn = maskTag.querySelector(".findBtn");
+const randBtn = maskTag.querySelector(".randBtn");
+const resetBtn = maskTag.querySelector(".resetBtn");
+const vowels2Btn = maskTag.querySelector(".vowels2Btn");
+const vowels3Btn = maskTag.querySelector(".vowels3Btn");
+const wordsTag = document.querySelector(".words");
+const abcTrue = document.querySelector(".charsTrue");
+const abcFalse = document.querySelector(".charsFalse");
+const abc = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+const charsF = document.getElementsByClassName("char_f");
+const charsT = document.getElementsByClassName("char_t");
+let totalWords = [];
+
+for (let i = 0; i < abc.length; i++) {
+	abcTrue.innerHTML += `<div class="char char_t">${abc[i]}</div>`;
+	abcFalse.innerHTML += `<div class="char char_f">${abc[i]}</div>`;
+}
+let findWords = [];
+fetch("./words.json")
+	.then(res => res.json())
+	.then(words => {
+		words.sort();
+		totalWords = [...words];
+		findWords = findByChars(words);
+		setCaption(caption, findWords);
+		setWords(wordsTag, findWords);
+		findBtn.addEventListener("click", e => {
+			let data = mask.value;
+			const reg = new RegExp(data.replace(/\*/g, "."))
+			totalWords = words.filter(w => reg.test(w));
+			findWords = findByChars(totalWords)
+			setCaption(caption, findWords);
+			setWords(wordsTag, findWords);
+		})
+		randBtn.addEventListener("click", e => {
+			const n = getRandom(totalWords.length);
+			mask.value = totalWords[n];
+			findBtn.click();
+		})
+		resetBtn.addEventListener("click", e => {
+			totalWords = [...words];
+			findWords = findByChars(words);
+			setCaption(caption, findWords);
+			setWords(wordsTag, findWords);
+		})
+		vowels3Btn.addEventListener("click", e => {
+			totalWords = findWithVowels(3, words)
+			findWords = findByChars(totalWords)
+			setCaption(caption, findWords);
+			setWords(wordsTag, findWords);
+		})
+		vowels2Btn.addEventListener("click", e => {
+			totalWords = findWithVowels(2, words)
+			findWords = findByChars(totalWords)
+			setCaption(caption, findWords);
+			setWords(wordsTag, findWords);
+		})
+		for (let i = 0; i < abc.length; i++) {
+			charsF[i].addEventListener("click", () => {
+				const cf = charsF[i];
+				const ct = charsT[i];
+				!cf.classList.contains("disable") && cf.classList.toggle("active");
+				cf.classList.contains("active")
+					? ct.classList.add("disable")
+					: ct.classList.remove("disable");
+				findWords = findByChars(totalWords)
+				setCaption(caption, findWords);
+				setWords(wordsTag, findWords);
+			})
+			charsT[i].addEventListener("click", () => {
+				const cf = charsF[i];
+				const ct = charsT[i];
+				!ct.classList.contains("disable") && ct.classList.toggle("active");
+				ct.classList.contains("active")
+					? cf.classList.add("disable")
+					: cf.classList.remove("disable");
+				findWords = findByChars(totalWords)
+				setCaption(caption, findWords);
+				setWords(wordsTag, findWords);
+			})
+		}
+	})
+
+
