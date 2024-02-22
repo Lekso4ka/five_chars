407e7df6b19c9200f6faa3154633660545c299c6
surnachevaad <Aleksandra.Surnacheva@lukoil.com>
2024-02-22 20:41:44 +0300

    refresh broke.js

--- rm.js
+++ rm.js
@@ -11,3 +11,10 @@ words = words.filter(w => !data.includes(w));
 console.log(data.length, words.length)
 
 fs.writeFileSync("./words_2.json", JSON.stringify(words), "utf-8");
+
+// feb 1 - feb 22
+//const average = [133, 88, 49, 74, 179, 14, 16, 262, 45, 345, 246, 209, 43, 347, 299, 539, 11, 218, 138, 30, 200];
+//
+//let sum = 0;
+//average.forEach(n => sum += n);
+//console.log(sum / average.length);
